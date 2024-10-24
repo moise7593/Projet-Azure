1a41f0d72cb76d70dd4f3df72f6dd418032d306c
moise7593 <moise.fernandez@estiam.com>
2024-10-24 11:18:06 +0200

    Update React.js

--- React.js
+++ React.js
@@ -1,9 +1,10 @@
+const http = require('http');
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello Projet\n');
+res.end('Projet-Azure-Fernandez\n');
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
 console.log(`Server running at http://localhost:${port}/`);
-})
+});
